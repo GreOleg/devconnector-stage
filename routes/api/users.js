501512d79a8751aa42a544f5dcfa4fcf542dd159
GreOleg <gresik.oleg@yandex.by>
2020-06-23 20:52:55 +0300

    created user schema

---
+++ routes/api/users.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
+
+// @route POST api/users
+// @desc Register user
+// @access Public
+router.get('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+],
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+    });
+
+module.exports = router;
