2cac82a5f13b53daeb52c8e5060bce0e7a41b58c
GreOleg <gresik.oleg@yandex.by>
2020-06-26 21:08:21 +0300

    made schema of profile

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 const authMiddleware = require('../../middleware/auth')
@@ -22,5 +26,58 @@ router.get('/', authMiddleware, async (req, res) => {
     }
 
 });
+
+
+// @route POST api/auth
+// @desc Authenticate user & get token
+// @access Public
+
+router.post('/', [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').not().isEmpty(),
+],
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        //res.status(200).json(req.body);
+
+        const { email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });  // email:email
+
+            if (!user) {
+                return res.status(400).json({ errors: [{ msg: 'Invalid credantials' }] });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res.status(400).json({ errors: [{ msg: 'Invalid credantials' }] });
+            }
+
+            const payload = {
+                user: {
+                    id: user.id,
+                },
+            };
+
+            jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 36000 }, (err, token) => {
+                if (err) {
+                    throw err
+                };
+                res.json({ token });
+            });
+
+        } catch (err) {
+            console.log(err.message);
+
+            res.status(200).send('Send error');
+        }
+    });
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route GET api/profile
-// @desc Test route
-// @access Public
-router.get('/', (req, res) => {
-    res.send('Profile route')
-});
+const authMiddleware = require('../../middleware/auth');
+
+// @route POST api/profile
+// @desc Create or update profile
+// @access Private
+
+router.post(
+    '/',
+    [authMiddleware, [
+        check('status', 'Status is required').not().isEmpty(),
+        check('skills', 'Skills are required').not().isEmpty(),
+    ],
+    ], async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        
+        const {
+            company,
+            website,
+            location,
+            bio,
+            status,
+            githubusername,
+            skills,
+            youtube,
+        } = req.body;
+
+        const profileFields = {};
+        profileFields.user = req.user.id;
+
+    });
 
 module.exports = router;
